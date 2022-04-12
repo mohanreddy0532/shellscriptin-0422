194acf88e364f51263752604a89f0f3b76814cfd
mohan <mohanreddy0532@gmail.com>
2022-04-12 17:58:12 +0530

    shell

--- 02-print.sh
+++ 02-print.sh
@@ -4,3 +4,16 @@ echo Hello World
 #Example
 echo Install nginx
 echo starting nginx service
+
+#Enable cloring in shell
+#echo -e "\e[COL=Message\e[0m"
+
+## -e to enable \e
+##  \e[  to enable colors
+##  COL Color code
+## m - End of syntax
+## 0 - disable color
+
+##Color codes
+
+echo -e "\e[31mHello in Red\e[0m"
