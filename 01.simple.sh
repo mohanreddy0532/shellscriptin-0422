c19883e9b890abf92e0466a3c40664f004bc9810
mohan <mohanreddy0532@gmail.com>
2022-04-21 10:44:43 +0530

    shell robosshop

--- 01.simple.sh
+++ 01.simple.sh
@@ -2,3 +2,9 @@
 
 uname -a;
 ls;
+
+USER_UID=$(id -u)
+if [ ${USER_UID} -ne 0 ]; then
+  echo -e "\e[1;31You should be a root user\e[0m"
+  exit
+fi
--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -2,18 +2,19 @@
 
 USER_UID=$(id -u)
 if [ ${USER_UID} -ne 0 ]; then
-  echo -e "\e[1;31mYou should be a root user to perform this script\e[0m"
+  echo -e "\e[1;31You should be a root user\e[0m"
   exit
 fi
 
-export COMPONENT=$1
+
+COMPONENT=$1
 if [ -z "$COMPONENT" ]; then
-  echo -e "\e[1;31mComponent INput Missing\e[0m"
+  echo -e "\e[1;31mComponent Input Missing\e[0m"
   exit
 fi
 
 if [ ! -e components/${COMPONENT}.sh ]; then
-  echo -e "\e[1;31mGiven component script does not exists\e[0m"
+  echo -e "\e[1;31mGiven component doesnont exist\e[0m"
   exit
 fi
 
